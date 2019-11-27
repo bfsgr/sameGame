f7577d8e075937508c8f3badeb9a7b7a938805cb
bfsgr <fusiegerb@gmail.com>
2019-11-26 22:37:47 -0300

    deslocamentos

--- same.c
+++ same.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sodium.h>
 #include "./filaPonteiro.c"
+#include "./pilhaPonteiro.c"
 
 void imprime(uint32_t matrix[][16]){
 	printf("\033[0mX  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15\n");
@@ -95,6 +96,50 @@ void buscaLargura(uint32_t matrix[][16], ixj entrada){
 	}
 }
 
+void deslocaBaixo(u_int32_t matrix[][16], Pilha *colVazia){
+	//descola pra baixo
+	inicializaPilha(colVazia);
+	for(int col = 15; col >= 0; col--){
+		Fila q;
+		inicializaFila(&q);
+		for(int lin = 11; lin >= 0; lin--){
+			if(matrix[lin][col] != 0){
+				ixj hold;
+				hold.i = matrix[lin][col];
+				insereFila(&q, hold);
+				matrix[lin][col] = 0;
+			}
+		}
+		for(int lin = 11; lin >= 0 && !filaVazia(&q); lin--){
+			ixj volta;
+			removeFila(&q, &volta);
+			matrix[lin][col] = volta.i;
+		}
+		if(matrix[11][col] == 0){
+			empilha(colVazia, col);
+		}	
+	}
+}
+void deslocaLado(u_int32_t matrix[][16]){
+	for(int lin = 11; lin >= 0; lin--){
+		Fila q;
+		inicializaFila(&q);
+		for(int col = 0; col < 16; col++){
+			if(matrix[lin][col] != 0){
+				ixj hold;
+				hold.i = matrix[lin][col];
+				insereFila(&q, hold);
+				matrix[lin][col] = 0;
+			}
+		}
+		for(int col = 0; col < 16 && !filaVazia(&q); col++){
+			ixj volta;
+			removeFila(&q, &volta);
+			matrix[lin][col] = volta.i;
+		}	
+	}
+}
+
 
 int main(){
 	//inicializa o libsodium
@@ -104,6 +149,9 @@ int main(){
 
     uint32_t matrix[12][16];
     ixj indice;
+
+	Pilha colunasVazias;
+
     geraMatriz(matrix);    
     imprime(matrix);
     
@@ -112,7 +160,10 @@ int main(){
 		scanf("%i,%i", &indice.i, &indice.j);
 
 		buscaLargura(matrix, indice);
-
+		deslocaBaixo(matrix, &colunasVazias);
+		if(!pilhaVazia(&colunasVazias)){
+			deslocaLado(matrix);
+		}
 		imprime(matrix);
 	}
     
