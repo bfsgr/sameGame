e423ed2939c58e01e33f538ff5ffddfb51b404f1
bfsgr <fusiegerb@gmail.com>
2019-11-30 00:16:18 -0300

    verifica vitoria e derrota

--- same.c
+++ same.c
@@ -54,7 +54,7 @@ void geraMatriz(uint32_t matrix[][16]){
     }
 }
 
-void buscaLargura(uint32_t matrix[][16], ixj entrada){
+int buscaLargura(uint32_t matrix[][16], ixj entrada, int pesquisa){
 	Fila q;
 	inicializaFila(&q);
 	insereFila(&q, entrada);
@@ -66,37 +66,55 @@ void buscaLargura(uint32_t matrix[][16], ixj entrada){
 			ixj noVizinho;
 			removeFila(&q, &no);
 			if(no.i+1 < 12 && corEntrada == matrix[no.i+1][no.j]){
-				matrix[no.i+1][no.j] = 0;
-				noVizinho.i = no.i + 1;
-				noVizinho.j = no.j;
-				insereFila(&q, noVizinho);
+				if(pesquisa == 1){
+					return 1;
+				} else {
+					matrix[no.i+1][no.j] = 0;
+					noVizinho.i = no.i + 1;
+					noVizinho.j = no.j;
+					insereFila(&q, noVizinho);
+				}
 			}
 			if(no.i-1 >= 0 && corEntrada == matrix[no.i-1][no.j]){
+				if(pesquisa == 1){
+					return 1;
+				} else {
 				matrix[no.i-1][no.j] = 0;
 				noVizinho.i = no.i - 1;
 				noVizinho.j = no.j;
 				insereFila(&q, noVizinho);
+				}
 			}
 			if(no.j+1 < 16 && corEntrada == matrix[no.i][no.j+1]){
-				matrix[no.i][no.j+1] = 0;
-				noVizinho.i = no.i;
-				noVizinho.j = no.j + 1;
-				insereFila(&q, noVizinho);
+				if(pesquisa == 1){
+					return 1;
+				} else {
+					matrix[no.i][no.j+1] = 0;
+					noVizinho.i = no.i;
+					noVizinho.j = no.j + 1;
+					insereFila(&q, noVizinho);
+				}
 			}
 			if(no.j-1 >= 0 && corEntrada == matrix[no.i][no.j-1]){
-				matrix[no.i][no.j-1] = 0;
-				noVizinho.i = no.i;
-				noVizinho.j = no.j - 1;
-				insereFila(&q, noVizinho);
+				if(pesquisa == 1){
+					return 1;
+				} else {
+					matrix[no.i][no.j-1] = 0;
+					noVizinho.i = no.i;
+					noVizinho.j = no.j - 1;
+					insereFila(&q, noVizinho);
+				}
 			}
-			if(!filaVazia(&q)){
+			if(!filaVazia(&q) && pesquisa == 0){
 				matrix[no.i][no.j] = 0;
 			}
 		}
+		return 0;
 	}
+	return 0;
 }
 
-void deslocaBaixo(u_int32_t matrix[][16]){
+void deslocaBaixo(uint32_t matrix[][16]){
 	//descola pra baixo
 	for(int col = 15; col >= 0; col--){
 		Fila q;
@@ -118,7 +136,8 @@ void deslocaBaixo(u_int32_t matrix[][16]){
 }
 
 
-void deslocaLado(u_int32_t matrix[][16]){
+void deslocaLado(uint32_t matrix[][16], int *venceu){
+	*venceu = 0;
 	for(int col = 0; col < 16; col++){
 		for(int i = col; i < 16; i++){
 			if(matrix[11][col] == 0 && col+1 < 16 && matrix[11][i] != 0){
@@ -128,11 +147,74 @@ void deslocaLado(u_int32_t matrix[][16]){
 				}
 			}
 		}
-		
-
+		*venceu = *venceu + matrix[11][col];
 	}
 }
 
+int checaValidade(uint32_t matrix[][16]){
+	for(int i = 11; i >= 0; i--){
+		for(int j = 0; j < 16; j++){
+			ixj teste;
+			teste.i = i;
+			teste.j = j;
+			if(buscaLargura(matrix, teste, 1) == 1){
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+void clean_stdin(void){
+  int c;
+  do {
+      c = getchar();
+  } while (c != '\n' && c != EOF);
+}
+
+int jogadenovo(){
+	char a;
+	clean_stdin();
+	scanf("%c", &a);
+	if(a == 'n'){
+		return 1;
+	} else {
+		iniciaJogo();
+	}
+}
+
+int iniciaJogo(){
+	uint32_t matrix[12][16];
+    ixj indice;
+	int jogadasValidas = 1, venceu = 1;
+
+    geraMatriz(matrix);    
+    imprime(matrix);
+    
+	while(jogadasValidas == 1 && venceu != 0){
+		printf("\nEscolha um indice: ");
+		scanf("%i,%i", &indice.i, &indice.j);
+
+		buscaLargura(matrix, indice, 0);
+		deslocaBaixo(matrix);
+		deslocaLado(matrix, &venceu);
+		if(venceu != 0){
+			jogadasValidas = checaValidade(matrix);
+		}
+		imprime(matrix);
+	}
+	if(venceu == 0){
+		printf("\nVocê ganhou. Gostaria de jogar de novo? (S/n) ");
+		if(jogadenovo()){
+			return 1;
+		}
+	} else {
+		printf("\nVocê perdeu. Gostaria de jogar de novo? (S/n) ");
+		if(jogadenovo()){
+			return 1;
+		}
+	}
+}
 
 int main(){
 	//inicializa o libsodium
@@ -140,21 +222,6 @@ int main(){
         return -1;
     }
 
-    uint32_t matrix[12][16];
-    ixj indice;
+	iniciaJogo(); 
 
-
-    geraMatriz(matrix);    
-    imprime(matrix);
-    
-	while(1){
-		printf("\nEscolha um indice: ");
-		scanf("%i,%i", &indice.i, &indice.j);
-
-		buscaLargura(matrix, indice);
-		deslocaBaixo(matrix);
-		deslocaLado(matrix);
-		imprime(matrix);
-	}
-    
 }
